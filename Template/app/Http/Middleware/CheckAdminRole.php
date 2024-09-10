f986e1488a2378c3a551ffea3720988470f893b3
fanyahMi <119622786+fanyahMi@users.noreply.github.com>
2024-09-10 07:24:33 +0200

    template_simple

---
+++ Template/app/Http/Middleware/CheckAdminRole.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+
+class CheckAdminRole
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
+     * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        if ($request->session()->has('role') && $request->session()->get('role') == 1) {
+            return $next($request);
+        }
+        abort(403, "Vous n'avez pas accès a cette page . Cette page est pour l' administrateur seulement ");
+    }
+}
