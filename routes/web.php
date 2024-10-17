dff6def5e76be2a68b273f80675fdc005ce79f25
fanyahMi <119622786+fanyahMi@users.noreply.github.com>
2024-10-18 01:03:10 +0200

    ao lo

--- routes/web.php
+++ routes/web.php
@@ -35,6 +35,7 @@ Route::middleware(['utilisateur'])->group(function () {
 
     Route::get('/list-compagne', [CompagneController::class, 'compagne']);
     Route::post('/ajout-compagne', [CompagneController::class, 'addAnnee_compagne']);
+    Route::get('/get-compagne', [CompagneController::class, 'getCompagne']);
 
 
     Route::get('/list-navire', [NavireController::class, 'index']);
